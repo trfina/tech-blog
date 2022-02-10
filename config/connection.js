43bdb2ccd9b7215bb81526e1b38bb7f6d8730290
Tammy Fina <finatammy@gmail.com>
2022-02-10 13:43:34 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize;
+let sequelize
 
 /// create connection to our database, pass in your MySQL information for username and password
 if (process.env.JAWSDB_URL) {
